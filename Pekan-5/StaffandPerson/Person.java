f12d4eaab0066331ef46ec7ef5512bf89a9ba229
Ahmad Agung Zefi Syahputra <56007263+ahmadagungzs@users.noreply.github.com>
2020-03-07 11:20:32 +0700

    Create Person.java

---
+++ Pekan-5/StaffandPerson/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    public String x = "person";
+    public void makan(){
+        System.out.println("Person makan");
+    }
+    public void minum(){
+        System.out.println("Person minum");
+    }
+}
