5a8954a9ebec7a7aa8c3d4e582d282d6b5856fa4
Ahmad Agung Zefi Syahputra <56007263+ahmadagungzs@users.noreply.github.com>
2020-03-07 11:28:56 +0700

    Update Mahasiswa.java

--- Pekan-4/Mahasiswa.java
+++ Pekan-4/Mahasiswa.java
@@ -1,1 +1,38 @@
+ public class Mahasiswa extends Orang {
 
+        private String NIP;
+        private String kelas;
+        private int semester;
+        private MahasiswaReport report;
+
+        public Mahasiswa(String nama, String alamat, String kota, int umur, char jenis_kelamin, String NIP, String kelas, int semester, MahasiswaReport Report) {
+                this.report=getReport();
+        }
+        public void setId(String id) { }
+        public void setkelas(String kelas) { }
+        public void setSemester(int semester) { }
+        public void setReport(MahasiswaReport report) { }
+        public String getNIP() {
+            return "";
+        }
+        public String getkelas()
+        {
+            return "";
+        }
+        public int getSemester()
+        {
+            return 0;
+        }
+        public MahasiswaReport getReport()
+        {
+            return report;
+        }
+        @Override
+        public String toString()
+        {
+        /*
+            Mengembalikan informasi mahasiswa
+         */
+            return "";
+        }
+}
